e051f3417b102e38c6ffc0f0528674d058d5b7d4
Antony Tri Susanto Itran <63346091+maslakonetop@users.noreply.github.com>
2021-12-21 16:19:43 +0700

    Add Task dan Bobot

---
+++ database/migrations/2021_12_21_091031_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_rekening');
+            $table->string('nama');
+            $table->string('deskripsi');
+            $table->date('target_tanggal_mulai');
+            $table->date('target_tanggal_selesai');
+            $table->date('tanggal_aktual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ database/migrations/2021_12_21_091442_create_bobots_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBobotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bobots', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_rekening');
+            $table->string('id_task');
+            $table->string('nama');
+            $table->string('deskripsi');
+            $table->string('progress');
+            $table->string('posisi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bobots');
+    }
+}
